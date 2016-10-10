af3a50f016eacdf631247da751a22fddfbe6f9ed
Tom McClelland <tom@mcclelland.me>
2016-10-09 21:49:30 -0400

    add image hover, fix validation issues, update gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -20,6 +20,15 @@
 
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var browserSync = require('browser-sync').create();
+
+gulp.task('browserSync', function() {
+    browserSync.init ( {
+        server: {
+            baseDir: "./"
+        }
+    });
+});
 
 gulp.task('sass', function () {
   return gulp.src('./css/**/*.scss')
@@ -27,8 +36,23 @@ gulp.task('sass', function () {
     .pipe(gulp.dest('./css'));
 });
 
+// Compile sass into CSS & auto-inject into browsers
+gulp.task('sass', function() {
+    return gulp.src("./css/**/*.scss")
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest("./css"))
+        .pipe(browserSync.stream());
+});
+
 gulp.task('sass:watch', function () {
   gulp.watch('./css/**/*.scss', ['sass']);
 });
 
-gulp.task('default', ['sass:watch']);
+gulp.task('serve', ['sass', 'browserSync'], function(){
+    gulp.watch("*.html").on('change', browserSync.reload);
+    gulp.watch("./css/**/*.scss", ['sass']);
+    // gulp.watch("css/*.css").on('change', browserSync.reload);
+});
+
+// gulp.task('default', ['sass:watch']);
+gulp.task('default', ['serve']);
